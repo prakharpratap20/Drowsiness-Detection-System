93fa512cdbecf506d2eb5cf763f664c896c045b0
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-08-28 13:16:23 +0530

    bug fix

--- dd.py
+++ dd.py
@@ -10,101 +10,106 @@ mixer.init()
 sound = mixer.Sound('alarm.wav')
 
 
-face = cv2.CascadeClassifier('haar/haarFF.xml')
-leye = cv2.CascadeClassifier('haar/haarLE.xml')
-reye = cv2.CascadeClassifier('haar/haarRE.xml')
+face = cv2.CascadeClassifier('haar/haarFF.xml')  # face detection
+leye = cv2.CascadeClassifier('haar/haarLE.xml')  # left eye detection
+reye = cv2.CascadeClassifier('haar/haarRE.xml')  # right eye detection
 
 
-lbl=['Close','Open']
+lbl = ['Close', 'Open']
 
 model = load_model('models/cnnCat2.h5')
 path = os.getcwd()
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-count=0
-score=0
-thicc=2
-rpred=[99]
+count = 0
+score = 0
+thicc = 2
+rpred = [99]
 rpred_class = 0
 lpred_class = 0
-lpred=[99]
+lpred = [99]
 
-while(True):
+while (True):  # loop to capture the video
     ret, frame = cap.read()
-    height,width = frame.shape[:2] 
+    height, width = frame.shape[:2]
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    faces = face.detectMultiScale(gray,minNeighbors=5,scaleFactor=1.1,minSize=(25,25))
+
+    faces = face.detectMultiScale(
+        gray, minNeighbors=5, scaleFactor=1.1, minSize=(25, 25))
     left_eye = leye.detectMultiScale(gray)
-    right_eye =  reye.detectMultiScale(gray)
+    right_eye = reye.detectMultiScale(gray)
 
-    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+    cv2.rectangle(frame, (0, height-50), (200, height),
+                  (0, 0, 0), thickness=cv2.FILLED)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
+    for (x, y, w, h) in faces:  # loop to detect the face
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 100, 100), 1)
 
-    for (x,y,w,h) in right_eye:
-        r_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
-        r_eye = cv2.resize(r_eye,(24,24))
-        r_eye= r_eye/255
-        r_eye=  r_eye.reshape(24,24,-1)
-        r_eye = np.expand_dims(r_eye,axis=0)
+    for (x, y, w, h) in right_eye:  # loop to detect the right eye
+        r_eye = frame[y:y+h, x:x+w]
+        count = count+1
+        r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye, (24, 24))
+        r_eye = r_eye/255
+        r_eye = r_eye.reshape(24, 24, -1)
+        r_eye = np.expand_dims(r_eye, axis=0)
         rpred = model.predict(r_eye)
         rpred_class = np.argmax(rpred)
         if rpred_class == 1:
-            lbl='Open' 
+            lbl = 'Open'
         if rpred_class == 0:
-            lbl='Closed'
+            lbl = 'Closed'
         break
 
-    for (x,y,w,h) in left_eye:
-        l_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
-        l_eye = cv2.resize(l_eye,(24,24))
-        l_eye= l_eye/255
-        l_eye=l_eye.reshape(24,24,-1)
-        l_eye = np.expand_dims(l_eye,axis=0)
+    for (x, y, w, h) in left_eye:  # loop to detect the left eye
+        l_eye = frame[y:y+h, x:x+w]
+        count = count+1
+        l_eye = cv2.cvtColor(l_eye, cv2.COLOR_BGR2GRAY)
+        l_eye = cv2.resize(l_eye, (24, 24))
+        l_eye = l_eye/255
+        l_eye = l_eye.reshape(24, 24, -1)
+        l_eye = np.expand_dims(l_eye, axis=0)
         lpred = model.predict(l_eye)
         lpred_class = np.argmax(lpred, axis=1)
         if lpred_class == 1:
-            lbl='Open'   
+            lbl = 'Open'
         if lpred_class == 0:
-            lbl='Closed'
+            lbl = 'Closed'
         break
 
-    if(rpred_class==0 and lpred_class==0):
-        score=score+1
-        cv2.putText(frame,"Closed",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    if (rpred_class == 0 and lpred_class == 0):  # if both eyes are closed
+        score = score+1
+        cv2.putText(frame, "Closed", (10, height-20), font,
+                    1, (255, 255, 255), 1, cv2.LINE_AA)
     # if(rpred[0]==1 or lpred[0]==1):
-    else:
-        score=score-1
-        cv2.putText(frame,"Open",(10,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-    
-        
-    if(score<0):
-        score=0   
-    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-    if(score>10):
-        #person is feeling sleepy so we beep the alarm
-        cv2.imwrite(os.path.join(path,'image.jpg'),frame)
+    else:  # if any eye is open
+        score = score-1
+        cv2.putText(frame, "Open", (10, height-20), font,
+                    1, (255, 255, 255), 1, cv2.LINE_AA)
+
+    if (score < 0):
+        score = 0
+    cv2.putText(frame, 'Score:'+str(score), (100, height-20),
+                font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    if (score > 10):
+        # person is feeling sleepy so we beep the alarm
+        cv2.imwrite(os.path.join(path, 'image.jpg'), frame)
         try:
             sound.play()
-            
+
         except:  # isplaying = False
             pass
-        if(thicc<16):
-            thicc= thicc+2
+        if (thicc < 16):
+            thicc = thicc+2
         else:
-            thicc=thicc-2
-            if(thicc<2):
-                thicc=2
-        cv2.rectangle(frame,(0,0),(width,height),(0,0,255),thicc) 
-    cv2.imshow('frame',frame)
+            thicc = thicc-2
+            if (thicc < 2):
+                thicc = 2
+        cv2.rectangle(frame, (0, 0), (width, height), (0, 0, 255), thicc)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
-cv2.destroyAllWindows()
+
+cap.release()  # release the camera
+cv2.destroyAllWindows()  # close the window
